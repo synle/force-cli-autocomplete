069daffb9f8333fde633641cfa61311c65503b5f
Sy Le <lenguyensy+xps@gmail.com>
2017-04-22 07:08:44 -0700

    help record

--- force-autocomplete-bash.sh
+++ force-autocomplete-bash.sh
@@ -2,11 +2,12 @@ _force()
 {
     local cur=${COMP_WORDS[COMP_CWORD]}
     local prev=${COMP_WORDS[COMP_CWORD-1]}
+    local prevprev=${COMP_WORDS[COMP_CWORD-2]}
 
     local ALL_COMMANDS="login logout logins active whoami describe sobject field record bulk fetch import export query apex log oauth test security version update push aura password notify limits help"
     local ALL_TYPES="actionLinkGroupTemplates analyticSnapshots applications appMenus approvalProcesses assignmentRules authproviders aura autoResponseRules callCenters cachePartitions certs channelLayouts classes communities components connectedApps corsWhitelistOrigins customApplicationComponents customMetadata customPermissions dashboards dataSources datacategorygroups delegateGroups documents EmbeddedServiceConfig email escalationRules feedFilters flexipages flowDefinitions flows globalPicklists groups homePageComponents homePageLayouts installedPackages labels layouts LeadConvertSettings letterhead matchingRules namedCredentials objects objectTranslations pages pathAssistants permissionsets postTemplates profiles postTemplates postTemplates profiles queues quickActions remoteSiteSettings reports reportTypes roles scontrols settings sharingRules siteDotComSites sites staticresources synonymDictionaries tabs triggers weblinks workflows cspTrustedSites"
     local ALL_HOST="mobile1.t.salesforce.com login.salesforce.com"
-
+    local ALL_STANDARD_OBJECTS="User Contact Lead Case Opportunity Account"
 
     case "$prev" in
         login)
@@ -30,6 +31,22 @@ _force()
         aura)
             ALL_PUSH_OPTS="push create delete list"
             COMPREPLY=( $(compgen -W "$ALL_PUSH_OPTS" -- $cur) )
+            return 0;
+        ;;
+        record)
+            COMPREPLY=( $(compgen -W "get create update delete" -- $cur) )
+            return 0;
+        ;;
+        create)
+            COMPREPLY=( $(compgen -W "$ALL_STANDARD_OBJECTS" -- $cur) )
+            return 0;
+        ;;
+        update)
+            COMPREPLY=( $(compgen -W "$ALL_STANDARD_OBJECTS" -- $cur) )
+            return 0;
+        ;;
+        delete)
+            COMPREPLY=( $(compgen -W "$ALL_STANDARD_OBJECTS" -- $cur) )
             return 0;
         ;;
         -i)
